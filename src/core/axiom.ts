23564292b34701437720624d8ab2da6999092466
Yu Jiang Tham <yoojbruin@gmail.com>
2023-06-05 08:25:18 +1000

    Remove WIP listener & transaction code

--- src/core/axiom.ts
+++ src/core/axiom.ts
@@ -2,8 +2,6 @@ import { Config } from '../shared/config';
 import { AxiomConfig } from '../shared/types';
 import { Block } from './block';
 import { QueryBuilder } from '../query/queryBuilder';
-import { listen } from './listener';
-import { sendQuery } from './transaction';
 import { decodePackedQuery } from '../query/decoder';
 
 export class Axiom {
@@ -17,22 +15,12 @@ export class Axiom {
   /**
    * Exported functions
    */
-  readonly listen: (events: string[], callback: (data: any) => void) => void;
-  readonly sendQuery: (queryResponse: string, refundee: string, query: string, callback: () => void) => void;
   readonly decodePackedQuery: (packedQuery: string) => string;
 
   constructor(config: AxiomConfig) {
     this.config = new Config(config);
 
     this.block = new Block(this.config);
-
-    this.listen = (events: string[], callback: (data: any) => void) => {
-      listen(this.config, events, callback);
-    }
-
-    this.sendQuery = (queryResponse: string, refundee: string, query: string, callback: () => void) => {
-      sendQuery(this.config, queryResponse, refundee, query, callback);
-    }
 
     this.decodePackedQuery = decodePackedQuery;
   }
