3ad19576f47457fd21bed6655d25edbd6ea217e6
Yu Jiang Tham <yoojbruin@gmail.com>
2023-06-05 08:23:04 +1000

    Update tsconfig

--- src/core/listener.ts
+++ src/core/listener.ts
@@ -4,21 +4,6 @@ import { Constants } from "../shared/constants";
 import { getAbiForVersion } from "./lib/abi";
 import { Config } from "../shared/config";
 
-// export class Listener {
-//   private readonly providerUri: string;
-  
-//   constructor(readonly config: AxiomConfig) {
-//     this.providerUri = config.providerUri;
-//   }
-
-//   async listen()
-// }
-
-/**
- * ax.listen(["e1", "e2"], callback)
- * ax.sendQuery(queryResponse, refundAddr, packedQueryBlob);
- */
-
 export function listen(config: Config, events: string[], callback: (...x: any) => void) {
   for (const event of events) {
     config.contract.on(event, callback);
