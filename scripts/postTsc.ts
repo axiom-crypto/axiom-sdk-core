3287eedf902590949e724767115f753471be4336
Yu Jiang Tham <ytham@users.noreply.github.com>
2023-06-23 15:22:43 -0700

    Create publish-ready package.json and configuration scripts (#28)
    * Update publish config
    
    * Update scripts
    
    * Add comments for pre/post tsc scripts

---
+++ scripts/postTsc.ts
@@ -0,0 +1,15 @@
+// Functions that are to be run after the typescript compiler runs
+
+const fs = require('fs');
+const packageJson = require('../package.json');
+
+// Copies a modified version of package.json to the /dist folder
+function copyPackageJson() {
+  const packageJsonCopy: any = { ...packageJson };
+  delete packageJsonCopy.scripts;
+  delete packageJsonCopy.devDependencies;
+  delete packageJsonCopy.publishConfig;
+  fs.writeFileSync('./dist/package.json', JSON.stringify(packageJsonCopy, null, 2));
+}
+
+copyPackageJson();
