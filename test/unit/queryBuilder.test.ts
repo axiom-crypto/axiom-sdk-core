a09784ff9dffe0e558f1f7007c4762b88ee4674f
Yu Jiang Tham <yoojbruin@gmail.com>
2023-06-28 17:25:49 -0700

    Update comment

--- test/unit/queryBuilder.test.ts
+++ test/unit/queryBuilder.test.ts
@@ -56,8 +56,8 @@ describe('QueryBuilder', () => {
     });
 
     test('should successfully build a Query with blockNumbers that contain a leading zero', async () => {
-      // NOTE: Alchemy will automatically format the hex blockNumber to remove the leading zero, 
-      // whereas Infura will not, so the test will always pass with Alchemy.
+      // NOTE: Alchemy automatically formats the hex blockNumber to remove the leading zero, 
+      // whereas Infura does not.
       const qb = ax.newQueryBuilder();
       await qb.append({blockNumber: 1808});   // 0x0710
       await qb.append({blockNumber: 4095});   // 0x0fff
