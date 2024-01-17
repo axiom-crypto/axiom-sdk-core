a2c5382f54e930eccbf8c4b830eac2b591cdd973
Yu Jiang Tham <yoojbruin@gmail.com>
2024-01-17 11:54:10 -0800

    Change v1 address back

--- src/shared/chainConfig/mainnet.ts
+++ src/shared/chainConfig/mainnet.ts
@@ -93,8 +93,8 @@ export let versionDataMainnet: any = {
 export let versionOverrideMainnetMock: any = {
   v1: {
     Addresses: {
-      Axiom: "",
-      AxiomQuery: "",
+      Axiom: "0x33ea514cc54b641ad8b84e4a31d311f3722d1bb5",
+      AxiomQuery: "0xd617ab7f787adf64c2b5b920c251ea10cd35a952",
     },
   },
 };
