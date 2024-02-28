2bc0679c6e9dbab21b2a22512b0a2eea81fb4250
Yu Jiang Tham <ytham@users.noreply.github.com>
2024-02-28 14:32:58 -0500

    Reduce scope of sdk-core and remove extraneous v1 items (#71)
    * WIP: Reduce scope; codec passthrough
    
    * Separate out tools export to another PR
    
    * Remove unused items
    
    * Remove v1
    
    * 2.3.6-rc.1
    
    * Add additional tools functions
    
    * rc.2
    
    * Fix import
    
    * Fix github workspaces env import
    
    * Update keys
    
    * Remove integration tests (move to sdk-client)
    
    * Update axiom-tools version

--- src/core/lib/abi.ts
+++ src/core/lib/abi.ts
@@ -1,13 +1,10 @@
 import { ethers } from "ethers";
-import AxiomV1QueryAbi from "./abi/AxiomV1Query.json";
 import AxiomV2QueryAbi from "./abi/AxiomV2Query.json";
 
 export function getAxiomQueryAbiForVersion(
   version: string
 ): ethers.InterfaceAbi {
   switch (version) {
-    case "v1":
-      return AxiomV1QueryAbi.abi;
     case "v2":
       return AxiomV2QueryAbi.abi;
     default:
--- src/shared/chainConfig/goerli.ts
+++ src/shared/chainConfig/goerli.ts
@@ -2,68 +2,6 @@ import { AxiomV2CircuitConstant } from "@axiom-crypto/tools";
 import { Endpoints, Path } from "../endpoints";
 
 export let versionDataGoerli: any = {
-  v0: {
-    Addresses: {
-      Axiom: "",
-    },
-    Urls: {
-      ApiBaseUrl: "https://api.axiom.xyz/v0",
-    },
-    Endpoints: {
-      GetBlockHashWitness: Endpoints.GetBlockHashWitness,
-      GetBlockMerkleProof: Endpoints.GetBlockMerkleProof,
-      GetBlockParams: Endpoints.GetBlockParams,
-      GetBlockRlpHeader: Endpoints.GetBlockRlpHeader,
-    },
-    Values: {
-      MaxQuerySize: 64,
-      QueryEncodingVersion: 1,
-    },
-  },
-  v0_2: {
-    Addresses: {
-      Axiom: "",
-    },
-    Urls: {
-      ApiBaseUrl: "https://api.axiom.xyz/v0_2",
-    },
-    Endpoints: {
-      GetBlockHashWitness: Endpoints.GetBlockHashWitness,
-      GetBlockMerkleProof: Endpoints.GetBlockMerkleProof,
-      GetBlockParams: Endpoints.GetBlockParams,
-      GetBlockRlpHeader: Endpoints.GetBlockRlpHeader,
-      GetBlockMmrProof: Endpoints.GetBlockMmrProof,
-    },
-    Values: {
-      MaxQuerySize: 64,
-      QueryEncodingVersion: 1,
-    },
-  },
-  v1: {
-    Addresses: {
-      Axiom: "0x8d41105949fc6C418DfF1A76Ff5Ae69128Ade55a",
-      AxiomQuery: "0x4Fb202140c5319106F15706b1A69E441c9536306",
-    },
-    Urls: {
-      ApiBaseUrl: "https://api.axiom.xyz/v1",
-    },
-    Endpoints: {
-      GetBlockHashWitness: Path.Block + Endpoints.GetBlockHashWitness,
-      GetBlockMerkleProof: Path.Block + Endpoints.GetBlockMerkleProof,
-      GetBlockParams: Path.Block + Endpoints.GetBlockParams,
-      GetBlockRlpHeader: Path.Block + Endpoints.GetBlockRlpHeader,
-      GetBlockMmrProof: Path.Block + Endpoints.GetBlockMmrProof,
-      GetAllQueries: Path.Query + Endpoints.GetAllQueries,
-      GetDataForQuery: Path.Query + Endpoints.GetDataForQuery,
-      GetQueryCount: Path.Query + Endpoints.GetQueryCount,
-      GetQueryData: Path.Query + Endpoints.GetQueryData,
-      GetQuery: Path.Query + Endpoints.GetQuery,
-    },
-    Values: {
-      MaxQuerySize: 64,
-      QueryEncodingVersion: 1,
-    },
-  },
   v2: {
     Addresses: {
       Axiom: "0x071b7aA74f060B40cB7EEE577c30E634276e7C9f",
@@ -92,12 +30,6 @@ export let versionDataGoerli: any = {
 };
 
 export let versionOverrideGoerliMock: any = {
-  v1: {
-    Addresses: {
-      Axiom: "0x8d41105949fc6C418DfF1A76Ff5Ae69128Ade55a",
-      AxiomQuery: "0x4Fb202140c5319106F15706b1A69E441c9536306",
-    },
-  },
   v2: {
     Addresses: {
       Axiom: "0x2aE6ad6127C222f071C023086C442080B03AfCCe",
